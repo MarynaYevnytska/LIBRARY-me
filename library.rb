62620d85b25fd517b1bdab876e3846ca0ee4d48e
Yevnytska Maryna <me_home@ukr.net>
2018-11-06 21:48:46 +0200

    Add new user without unique ID, validation existing user don't execute

--- library.rb
+++ library.rb
@@ -1,81 +1,75 @@
 
 
 class Autor
-attr_reader :name, :biographi
+  attr_reader :name, :biographi
 
-  def initialize (name = "Autor name", biographi)
-    @name, @biographi = name, biographi
-  end
+    def initialize (name = "Autor name", biographi)
+      @name, @biographi = name, biographi
+    end
 end
 
 
 class Book
 end
 
+
 class Reader
   attr_accessor :name, :email, :city, :street, :house
+  require 'json'
     def initialize (name, email, city, street, house)
       @name, @email, @city, @street, @house  = name, email, city, street, house
     end
 
     def add_new_reader
-        puts "!!!!WOW!!!! #{@name}"
+      readers = {name:@name, email: @email,city: @city, street: @street, house:@house }
+      string = readers.to_json
+      puts string
+      File.open("1_Readers.txt",'a') {|file| file.puts string}
+      puts "ADD"
+      puts "!!!!WOW!!!! We are so glad than you #{@name} will be a part of our community, you email is #{@email}, you live in #{@city},on the street #{@street}, in the house #{@house}"
+    end
+
+      def check_old_reader
+        string = "{\"name\":\"#{@name}\",\"email\":\"#{@email}\",\"city\":\"#{@city}\",\"street\":\"#{@street}\",\"house\":\"#{@house}\"}"
+        reders_list = File.open("1_Readers.txt", 'r') {|file| file.readlines}
+        puts a=reders_list.size
+        puts reders_list[0].class
+        puts reders_list[-1]
+        puts string
+        c =reders_list[-1].size
+        c_1 = string.size
+        puts c==c_1
+        puts string==reders_list[-1]
+        b = reders_list.include?(string)
+        puts b
+        end
+end
+
+#class Order
+#end
+
+def questions
+   print yield
+  answer = gets.chomp
+end
+
+puts "Hello, I am Marina, glad to see you into our Library"
+new_old = questions {"Are you first time there? (Enter Yes or No):"}
+name = questions {"Enter you name:"}
+email = questions {"Enter you email:"}
+city  = questions {"Enter you city:"}
+street = questions {"Enter you street:"}
+house = questions {"Enter you house:"}
+reader = Reader.new(name, email,city, street, house)
+
+if new_old=="yes" or new_old=="Yes"
+  join_new = questions { "Great! Do You want to join? (Enter Yes or No):"}
+    if join_new=="yes" or join_new=="Yes"
+      reader.add_new_reader
+      puts "Great! You were added to the most reading readers! "
       end
-end
-
-class Order
-end
-
-
-def questions_for_regestration_autorisation
-  reader_hash = {}
-  reader_ary = Array.new
-
-  print "Enter you name: "
-  name = gets.chomp
-  reader_hash[:name] = name
-  reader_ary.push(name)
-
-  print "Enter you email: "
-  email = gets.chomp
-  reader_hash[:email] = email
-  reader_ary.push(email)
-
-  print "Enter you city: "
-  city = gets.chomp
-  reader_hash[:city] = city
-  reader_ary.push(city)
-
-
-  print "Enter you street: "
-  street  = gets.chomp
-  reader_hash[:street] = street
-  reader_ary.push(street)
-
-  print "Enter you house: "
-  house  = gets.chomp
-  reader_hash[:house] = house
-  puts reader_hash, reader_ary
-  return reader_hash
-end
-
-
-puts "Are you first time there? (Enter Yes or No): "
-answer=gets.chomp
-
-
-if answer=="yes" or answer=="Yes"
-  puts "Great! You can join to the most reading readers! "
-  readers = questions_for_regestration_autorisation
-  puts readers
-  #new_reader = Reader.new (readers[:name], readers[:email] , readers[:city] ,readers[:street] , readers[:house])
-  #new_reader.add_new_reader
-  puts "Great! You were added to the most reading readers! "
-else
+  else
   puts "Welcom! Lets check  your information!"
-  questions_for_regestration_autorisation
-  puts "Welcom! Glad to see you again!"
+  reader.check_old_reader
 end
-
-
 puts "lets GO"
