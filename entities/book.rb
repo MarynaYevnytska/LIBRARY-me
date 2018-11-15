eee635be245281fad9f1b79db9b71b17dcf33c24
Yevnytska Maryna <me_home@ukr.net>
2018-11-15 13:15:50 +0200

    book changed

--- entities/book.rb
+++ entities/book.rb
@@ -3,11 +3,7 @@
 class Book
   attr_reader :title, :author
 
-  include Validation
-
   def initialize(title, author)
-    Validation.not_empty(title)
-    Validation.egual_string(title)
     @title = title
     @author = author
   end
