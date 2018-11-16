c3cd9cc4d8839d47a258bce59523334964cfdcd0
Yevnytska Maryna <me_home@ukr.net>
2018-11-16 14:22:17 +0200

    Check string and empty is correct

--- entities/author.rb
+++ entities/author.rb
@@ -2,23 +2,24 @@
 
 class Author
   attr_reader :name, :biography
-
-  def initialize(name, biography='')
+  def initialize(name, biography = '')
     validation(name, biography)
     @name = name
     @biography = biography
   end
-count=0
-  def not_empty(item)
-      puts "Yes #{item} is not empty" unless item.empty?
+
+  def not_empty(object)
+    puts "Yes #{object}is not empty" unless object.empty?
+  end
+
+  def validate_class(object, klass)
+    puts "Yes #{object} is #{klass}" if object.is_a? klass
+  end
+
+  def validation(*arguments)
+    arguments.each do |item|
+      validate_class(item, String)
+      not_empty(item)
     end
-  end
-  def validate_class(item,klass)
-    puts "Yes #{item} is #{klass}" if  item.is_a? klass
-  end
-  def validation (*arg)
-    arg.each do |item|
-    validate_class(item,String)
-    not_empty(item)
   end
 end
--- entities/main.rb
+++ entities/main.rb
@@ -8,6 +8,6 @@ count = 0
 names_author = ['Nic', 'Mat', 'David', 1, '']
 describ_biography = %w[Happyness Desert Love Freedom]
 names_author.each do |item|
-  author = Author.new"#{item}, #{describ_biography[count]}"
-  count +=1
+  author = Author.new(item, describ_biography[count])
+  count += 1
 end
